a080f185cf53322fa32715258ccf9dc4306d578a
omri wallach <omriwallach1992@gmail.com>
2021-01-11 15:54:20 +0100

    visualize k-means

--- k_means/main.py
+++ k_means/main.py
@@ -25,9 +25,17 @@ plt.show()
 
 # using the optimal number of clusters
 kmeans = model(n_clusters=5, random_state=42)
-kmeans.fit(X)
+y_pred = kmeans.fit_predict(X)
 
 # Visualizing the prediction
+import random
 
+plt.scatter(X[y_pred == 0, 0], X[y_pred == 0, 1], s=100, c="red", label="C1")
+plt.scatter(X[y_pred == 1,0], X[y_pred == 1,1],s=100, c="blue" , label="C2")
+plt.scatter(X[y_pred == 2,0], X[y_pred == 2,1],s=100, c="green" , label="C3")
+plt.scatter(X[y_pred == 3,0], X[y_pred == 3,1],s=100, c="yellow" , label="C4")
+plt.scatter(X[y_pred == 4, 0], X[y_pred == 4, 1], s=100, c="purple", label="C5")
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1],s=300,c="black",label="Centroids")
+plt.show()
 
 
